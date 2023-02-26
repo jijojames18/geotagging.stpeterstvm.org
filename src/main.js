ce9a3a4d6187ea916f01d5757eac70d21f2741a5
Jijo James <jijojames18@gmail.com>
2023-02-26 23:01:26 +0530

    Init firestore

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { initializeApp } from 'firebase/app'
+
+const app = initializeApp(import.meta.env.VITE_APP_FIREBASE_SDK_CONFIG)
+
 import './assets/main.scss'
 
 createApp(App).mount('#app')
